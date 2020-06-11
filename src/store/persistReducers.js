11c4962b7339bad84c0a0913fad88ec22bf23748
EmersonOliveiraDev <emersonhaw@gmail.com>
2020-06-11 12:12:17 -0300

    Add toastify and signIn logic

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -4,7 +4,7 @@ import { persistReducer } from 'redux-persist'
 export default (reducers) => {
   const persistedReduder = persistReducer(
     {
-      key: 'root',
+      key: 'gobarber',
       storage,
       whitelist: ['auth', 'user'],
     },
