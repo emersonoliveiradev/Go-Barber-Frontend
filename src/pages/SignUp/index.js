5b6cc1d3fad95b4873d28995965442cff64a0333
EmersonOliveiraDev <emersonhaw@gmail.com>
2020-06-10 23:24:53 -0300

    Add validation form

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Form, Input } from '@rocketseat/unform'
+import * as Yup from 'yup'
 
 import logo from '~/assets/logo.svg'
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha deve conter no mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+})
 
 function SignUp() {
   function handleSubmit(data) {
@@ -12,7 +23,7 @@ function SignUp() {
   return (
     <>
       <img src={logo} alt="Logo" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" type="text" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
