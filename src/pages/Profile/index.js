b814fe20b057daea02874e34e5949c3472a9644a
EmersonOliveiraDev <emersonhaw@gmail.com>
2020-06-13 04:09:50 -0300

    Add profile and fix show components default

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Form, Input } from '@rocketseat/unform'
 
-// import { Container } from './styles';
+import { Container } from './styles'
 
-function Profile() {
-  return <div>Profile</div>
+export default function Profile() {
+  const profile = useSelector((state) => state.user.profile)
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="Password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirme sua nova senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sairdo GoBarber</button>
+    </Container>
+  )
 }
-
-export default Profile
--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Header from '~/components/Header'
 
-import { Wrapper } from './styles'
+import { Wrapper, Container } from './styles'
 
 export default function DefaultLayout({ children }) {
   return (
     <Wrapper>
       <Header>{children}</Header>
+      <Container>{children}</Container>
     </Wrapper>
   )
+}
+
+DefaultLayout.propTypes = {
+  children: PropTypes.element.isRequired,
 }
